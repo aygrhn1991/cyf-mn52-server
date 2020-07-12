91153338624aaacb9206494c54f1b63d3f8c8b58
chenyufeng <411610370@qq.com>
2020-07-12 22:18:52 +0800

    1

--- src/test/java/com/cyf/mn52/Mn52ApplicationTests.java
+++ src/test/java/com/cyf/mn52/Mn52ApplicationTests.java
@@ -1,13 +1,70 @@
 package com.cyf.mn52;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.Callback;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
 
 @SpringBootTest
 class Mn52ApplicationTests {
 
     @Test
     void contextLoads() {
+        try{
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        String accessKeyId = "LTAI4G4ifSEhRjVSgQcUFYWW";
+        String accessKeySecret = "G7ams1ruiYx7FDAk9QI0wyt255egED";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 创建PutObjectRequest对象。
+        PutObjectRequest putObjectRequest = new PutObjectRequest("mn5200", "img/allimg/20200720/datav.png", new File("E:/datav.png"));
+
+// 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+// ObjectMetadata metadata = new ObjectMetadata();
+// metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+// metadata.setObjectAcl(CannedAccessControlList.Private);
+// putObjectRequest.setMetadata(metadata);
+
+// 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+// 上传回调参数。
+        Callback callback = new Callback();
+        callback.setCallbackUrl("http://127.0.0.1:8002/api/index");
+//（可选）设置回调请求消息头中Host的值，即您的服务器配置Host的值。
+// callback.setCallbackHost("yourCallbackHost");
+// 设置发起回调时请求body的值。
+        callback.setCallbackBody("{\\\"mimeType\\\":${mimeType},\\\"size\\\":${size}}");
+// 设置发起回调请求的Content-Type。
+        callback.setCalbackBodyType(Callback.CalbackBodyType.JSON);
+// 设置发起回调请求的自定义参数，由Key和Value组成，Key必须以x:开始。
+        callback.addCallbackVar("x:var1", "value1");
+        callback.addCallbackVar("x:var2", "value2");
+        putObjectRequest.setCallback(callback);
+
+        PutObjectResult putObjectResult = ossClient.putObject(putObjectRequest);
+
+// 读取上传回调返回的消息内容。
+        byte[] buffer = new byte[1024];
+        putObjectResult.getResponse().getContent().read(buffer);
+// 数据读取完成后，获取的流必须关闭，否则会造成连接泄漏，导致请求无连接可用，程序无法正常工作。
+        putObjectResult.getResponse().getContent().close();
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
 }
