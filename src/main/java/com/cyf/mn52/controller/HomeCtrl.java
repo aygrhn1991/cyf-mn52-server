e1b2fbd49693367075bd60d3d7195f8eff88c071
chenyufeng <411610370@qq.com>
2020-07-26 18:45:22 +0800

    1

--- src/main/java/com/cyf/mn52/controller/HomeCtrl.java
+++ src/main/java/com/cyf/mn52/controller/HomeCtrl.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,7 +52,7 @@ public class HomeCtrl {
     }
     //endregion
 
-    //region 页面
+    //region 接口
     //导航(栏目)/置顶标签
     @RequestMapping("/getLayoutData")
     @ResponseBody
@@ -64,6 +65,26 @@ public class HomeCtrl {
         list = this.jdbc.queryForList(sql);
         map.put("tag", list);
         return R.success("布局页数据", map);
+    }
+
+    @RequestMapping("/getIndexData")
+    @ResponseBody
+    public Result getIndexData() {
+        Map map = new HashMap();
+        String sql = "select t.* from mn_category t where t.state=1 order by t.sort desc";
+        List<Map<String, Object>> list = this.jdbc.queryForList(sql);
+        for (Map m : list) {
+            sql = "select *,group_concat(t1.tag_name) tags from mn_gallery t " +
+                    " right join (select tt.*,tt1.name tag_name from mn_gallery_tag tt left join mn_tag tt1 on tt.tag_id=tt1.id) t1 on t.id=t1.gallery_id " +
+                    " where t.state=1 and t.category_id=? group by t.id order by t.time_publish desc limit 0,4 ";
+            List<Map<String, Object>> galleryList = this.jdbc.queryForList(sql, m.get("id").toString());
+            m.put("gallery", galleryList);
+        }
+        map.put("category", list);
+        sql = "select t.* from mn_tag t where t.state=1 and t.top=1";
+        list = this.jdbc.queryForList(sql);
+        map.put("tag", list);
+        return R.success("首页数据", map);
     }
     //endregion
 }
