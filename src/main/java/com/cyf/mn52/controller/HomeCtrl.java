ac3b1d6b3c5ebf85a07d570604de73f5bf565cfd
chenyufeng <411610370@qq.com>
2020-07-28 21:01:54 +0800

    1

--- src/main/java/com/cyf/mn52/controller/HomeCtrl.java
+++ src/main/java/com/cyf/mn52/controller/HomeCtrl.java
@@ -5,10 +5,13 @@ import com.cyf.mn52.suit.response.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
@@ -22,6 +25,9 @@ public class HomeCtrl {
     @Autowired
     private JdbcTemplate jdbc;
 
+    @Value("${oss.url}")
+    private String ossUrl;
+
     @RequestMapping("/test/{message}")
     public Result test(@PathVariable String message) {
         return R.success("ok", message);
@@ -29,52 +35,7 @@ public class HomeCtrl {
 
     //region 页面
     @RequestMapping("/index")
-    public String index() {
-        return "home/index";
-    }
-
-    @RequestMapping("/category")
-    public String category() {
-        return "home/category";
-    }
-
-    @RequestMapping("/gallery")
-    public String gallery() {
-        return "home/gallery";
-    }
-
-    @RequestMapping("/tag")
-    public String thumb() {
-        return "home/tag";
-    }
-    //endregion
-
-    //region 接口
-    //导航(栏目)/置顶标签
-    @RequestMapping("/getLayoutData")
-    @ResponseBody
-    public Result getLayoutData() {
-        Map map = new HashMap();
-        String sql = "select t.* " +
-                "from mn_category t " +
-                "where t.state=1 " +
-                "order by t.sort desc";
-        List<Map<String, Object>> list = this.jdbc.queryForList(sql);
-        map.put("category", list);
-        sql = "select t.* " +
-                "from mn_tag t " +
-                "where t.state=1 " +
-                "and t.top=1";
-        list = this.jdbc.queryForList(sql);
-        map.put("tag", list);
-        return R.success("布局页数据", map);
-    }
-
-    //轮播/栏目列表及每个栏目下4个最新图集/热门标签
-    @RequestMapping("/getIndexData")
-    @ResponseBody
-    public Result getIndexData() {
-        Map map = new HashMap();
+    public String index(Model model) {
         String sql = "select t.id,t.name " +
                 "from mn_category t " +
                 "where t.state=1 " +
@@ -82,7 +43,7 @@ public class HomeCtrl {
         List<Map<String, Object>> categoryList = this.jdbc.queryForList(sql);
         for (Map category : categoryList) {
             sql = "select t2.id tag_id,t2.name tag_name,t3.*" +
-                    "from mn_gallery_tag t1,mn_tag t2,(select t.id,t.title,t.cover,t.scan,t.good,t.time_publish from mn_gallery t where t.category_id=? order by t.time_publish desc limit 0,4) t3 " +
+                    "from mn_gallery_tag t1,mn_tag t2,(select t.id,t.title,t.cover,t.scan,t.good,t.time_publish,(select count(*) from mn_image tt where tt.gallery_unique_id=t.unique_id) img from mn_gallery t where t.category_id=? order by t.time_publish desc limit 0,4) t3 " +
                     "where t1.tag_id=t2.id " +
                     "and t1.gallery_id=t3.id " +
                     "order by t3.time_publish desc";
@@ -112,14 +73,57 @@ public class HomeCtrl {
             }
             category.put("gallery", galleryList);
         }
-        map.put("category", categoryList);
+        model.addAttribute("category", categoryList);
         sql = "select t.id,t.name from mn_tag t where t.state=1 order by t.time_update desc limit 0,100";
         List<Map<String, Object>> tagList = this.jdbc.queryForList(sql);
-        map.put("tag", tagList);
+        model.addAttribute("tag", tagList);
         sql = "select t.id,t.title,t.cover from mn_gallery t where t.state=1 and t.carousel=1 order by t.time_publish desc";
         List<Map<String, Object>> carouselList = this.jdbc.queryForList(sql);
-        map.put("carousel", carouselList);
-        return R.success("首页数据", map);
+        model.addAttribute("carousel", carouselList);
+        Map layout = this.getLayoutData();
+        model.addAttribute("topCategory", layout.get("topCategory"));
+        model.addAttribute("topTag", layout.get("topTag"));
+        model.addAttribute("ossUrl", this.ossUrl);
+        model.addAttribute("date", new Date());
+        return "home/index";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String category(@PathVariable int id) {
+        return "home/category";
+    }
+
+    @RequestMapping("/gallery/{id}")
+    public String gallery(@PathVariable int id) {
+        return "home/gallery";
+    }
+
+    @RequestMapping("/tag/{id}")
+    public String tag(@PathVariable int id) {
+        return "home/tag";
+    }
+
+    @RequestMapping("/search")
+    public String search(@RequestParam int id, @RequestParam String key) {
+        return "home/search";
     }
     //endregion
+
+
+    private Map getLayoutData() {
+        Map map = new HashMap();
+        String sql = "select t.id,t.name " +
+                "from mn_category t " +
+                "where t.state=1 " +
+                "order by t.sort desc";
+        List<Map<String, Object>> list = this.jdbc.queryForList(sql);
+        map.put("topCategory", list);
+        sql = "select t.id,t.name " +
+                "from mn_tag t " +
+                "where t.state=1 " +
+                "and t.top=1";
+        list = this.jdbc.queryForList(sql);
+        map.put("topTag", list);
+        return map;
+    }
 }
